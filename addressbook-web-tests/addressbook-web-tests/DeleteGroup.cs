4023fbb62e93bc6145d93219ba6caeea58e8352c
Evgenii <ukladov.ev@gmail.com>
2021-07-30 19:44:21 +0300

    One more test and common class for all tests are created

---
+++ addressbook-web-tests/addressbook-web-tests/DeleteGroup.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace addressbook_web_tests
+{
+    [TestFixture]
+    public class DeleteGroup : TestBase
+    {
+        [Test]
+        public void TheDeleteGroupTest()
+        {
+            OpenPage();
+            loginLogoutHelper.Authorization(new AccountData("admin", "secret"));
+            navigationHelper.OpenGroupPage();
+            SelectingGroup(1);
+            RemoveGroup();
+            navigationHelper.ReturnGroupPage();
+            loginLogoutHelper.Logout();
+        }
+
+        private void RemoveGroup()
+        {
+            driver.FindElement(By.XPath("//div[@id='content']/form/input[5]")).Click();
+        }
+
+        private void SelectingGroup(int index)
+        {
+            driver.FindElement(By.XPath("//div[@id='content']/form/span[" + index + "]/input")).Click();
+        }
+    }
+}
---
+++ addressbook-web-tests/addressbook-web-tests/TestBase.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace addressbook_web_tests
+{
+    public class TestBase
+    {
+        protected IWebDriver driver;
+        private StringBuilder verificationErrors;
+        protected string baseURL;
+
+        protected LoginLogoutHelper loginLogoutHelper;
+        protected NavigationHelper navigationHelper;
+        protected FillingFormHelper fillingFormHelper;
+        [SetUp]
+        public void SetupTest()
+        {
+            driver = new ChromeDriver();
+            baseURL = "http://localhost/addressbook/";
+            verificationErrors = new StringBuilder();
+
+            loginLogoutHelper = new LoginLogoutHelper(driver); // добавили помощника для разгрузки TestBase
+            navigationHelper = new NavigationHelper(driver);
+            fillingFormHelper = new FillingFormHelper(driver);
+        }
+
+        [TearDown]
+        public void TeardownTest()
+        {
+            try
+            {
+                driver.Quit();
+            }
+            catch (Exception)
+            {
+                // Ignore errors if unable to close the browser
+            }
+            Assert.AreEqual("", verificationErrors.ToString());
+        }
+
+        protected void OpenPage()
+        {
+            driver.Navigate().GoToUrl(baseURL);
+        }
+
+    }
+}
